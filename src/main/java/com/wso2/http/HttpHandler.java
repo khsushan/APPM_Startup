446a8bf9188b525aff825e44a00012af01629ba3
khsushan <khsushan@gmail.com>
2015-02-20 16:16:31 +0530

    add mobile application mulidata

--- src/main/java/com/wso2/http/HttpHandler.java
+++ src/main/java/com/wso2/http/HttpHandler.java
@@ -1,14 +1,25 @@
 package com.wso2.http;
 
-import org.json.simple.JSONObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
-import javax.json.Json;
+
+import javax.activation.MimeType;
 import javax.net.ssl.HttpsURLConnection;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ProtocolException;
+import java.io.*;
 import java.net.URL;
 
 /**
@@ -19,7 +30,7 @@ public class HttpHandler {
     private final String USER_AGENT = "Mozilla/5.0";
 
 
-    public String doPost(String backEnd, String payload, String your_session_id) throws IOException {
+    public String doPost(String backEnd, String payload, String your_session_id,String contentType) throws IOException {
 
         URL obj = new URL(backEnd);
         HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
@@ -31,7 +42,7 @@ public class HttpHandler {
             con.setRequestProperty(
                     "Cookie", "JSESSIONID=" + your_session_id);
         }
-        con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        con.setRequestProperty("Content-Type", contentType);
         con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
 
         //String urlParameters = ;
@@ -73,7 +84,109 @@ public class HttpHandler {
 
         return  null;
         //print result
+    }
 
+    public String RestPutClient(String url,String cookie) {
+        // example url : http://localhost:9898/data/1d3n71f13r.json
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        StringBuilder result = new StringBuilder();
+        try {
+            HttpPut putRequest = new HttpPut(url);
+            putRequest.addHeader("Content-Type", "application/x-www-form-urlencoded");
+            putRequest.addHeader("Accept-Language", "en-US,en;q=0.5");
+            putRequest.addHeader("Cookie","JSESSIONID=" + cookie);
+            putRequest.addHeader("Accept-Encoding","gzip, deflate");
+            HttpResponse response = httpClient.execute(putRequest);
+            if (response.getStatusLine().getStatusCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + response.getStatusLine().getStatusCode());
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (response.getEntity().getContent())));
+            String output;
+            while ((output = br.readLine()) != null) {
+                result.append(output);
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
+
+
+    public String postMultiData(String url,String method,String appMeta,String cookie) throws IOException {
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost =  new HttpPost(url);
+        //httpPost.addHeader("Content-Type",contentType);
+        //httpPost.addHeader("Accept","application/json, text/javascript, */*; q=0.01");
+        //httpPost.addHeader("Accept-Encoding","gzip, deflate");
+        //httpPost.addHeader("Accept-Language","en-US,en;q=0.5");
+        httpPost.addHeader("Cookie", "JSESSIONID=" + cookie);
+
+        MultipartEntityBuilder reqEntity;
+        if(method.equals("upload")) {
+            System.out.println("Upload app data");
+            reqEntity = MultipartEntityBuilder.create();
+            FileBody fileBody = new FileBody(new File("/home/ushan/wso2/APPM/LATEST_BUILD/wso2appm-1.0.0-SNAPSHOT/repository/resources/mobileapps/CleanCalc/Resources/CleanCalc.apk"));
+
+            reqEntity.addPart("file", fileBody);
+        }else{
+            System.out.println("Send app data");
+            reqEntity = MultipartEntityBuilder.create();
+            reqEntity.addPart("version",new StringBody("1.2.3",ContentType.MULTIPART_FORM_DATA));
+            reqEntity.addPart("provider",new StringBody("1WSO2Mobile",ContentType.MULTIPART_FORM_DATA));
+            reqEntity.addPart("markettype",new StringBody("Enterprise",ContentType.MULTIPART_FORM_DATA));
+            reqEntity.addPart("platform",new StringBody("android",ContentType.MULTIPART_FORM_DATA));
+            reqEntity.addPart("name",new StringBody("myTest",ContentType.MULTIPART_FORM_DATA));
+            reqEntity.addPart("description",new StringBody("this is a clean calucultor",ContentType.MULTIPART_FORM_DATA));
+            FileBody bannerImageFile = new FileBody(new File("/home/ushan/Shell_Script_Test/APPM/wso2appm-1.0.0-SNAPSHOT/repository/resources/mobileapps/CleanCalc/Resources/banner.png"));
+            reqEntity.addPart("bannerFile",bannerImageFile);
+            FileBody iconImageFile = new FileBody(new File("/home/ushan/Shell_Script_Test/APPM/wso2appm-1.0.0-SNAPSHOT/repository/resources/mobileapps/CleanCalc/Resources/icon.png"));
+            reqEntity.addPart("iconFile",iconImageFile);
+            FileBody screenShot1 = new FileBody(new File("/home/ushan/Shell_Script_Test/APPM/wso2appm-1.0.0-SNAPSHOT/repository/resources/mobileapps/CleanCalc/Resources/screen1.png"));
+            reqEntity.addPart("screenshot1File",screenShot1);
+            FileBody screenShot2= new FileBody(new File("/home/ushan/Shell_Script_Test/APPM/wso2appm-1.0.0-SNAPSHOT/repository/resources/mobileapps/CleanCalc/Resources/screen2.png"));
+            reqEntity.addPart("screenshot2File",screenShot2);
+            FileBody screenShot3 = new FileBody(new File("/home/ushan/Shell_Script_Test/APPM/wso2appm-1.0.0-SNAPSHOT/repository/resources/mobileapps/CleanCalc/Resources/screen3.png"));
+            reqEntity.addPart("screenshot3File",screenShot3);
+
+
+            reqEntity.addPart("addNewAssetButton",new StringBody("Submit",ContentType.MULTIPART_FORM_DATA));
+            reqEntity.addPart("mobileapp",new StringBody("mobileapp",ContentType.MULTIPART_FORM_DATA));
+            reqEntity.addPart("appmeta",new StringBody(appMeta,ContentType.MULTIPART_FORM_DATA));
+        }
+        final HttpEntity entity = reqEntity.build();
+        httpPost.setEntity(entity);
+        System.out.println("Requesting : " + httpPost.getAllHeaders());
+
+
+
+
+        ResponseHandler<String> responseHandler = new BasicResponseHandler();
+        String responseBody = httpClient.execute(httpPost, responseHandler);
+        System.out.println("responseBody : " + responseBody);
+        int statusCode = -1;
+        /*String responseBody = null;
+
+        try {
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+
+            StatusLine status = httpResponse.getStatusLine();
+            statusCode = status.getStatusCode();
+            responseBody = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
+            System.out.println("Status code : "+ status);
+            System.out.println("Responce body : "+ responseBody);
+            if (statusCode >= 300) {
+
+            } else {
+
+            }
+        } catch (IOException e) {
+
+        }*/
+        return  responseBody;
 
     }
 }
--- src/main/java/com/wso2/main/MainController.java
+++ src/main/java/com/wso2/main/MainController.java
@@ -1,8 +1,10 @@
 package com.wso2.main;
 
+import com.sun.tools.xjc.reader.xmlschema.bindinfo.BIConversion;
 import com.wso2.appm.ClaimManagementServiceClient;
 import com.wso2.appm.LoginAdminServiceClient;
 import com.wso2.appm.RemoteUserStoreManagerServiceClient;
+import com.wso2.appm.WSRegistryService_Client;
 import com.wso2.bean.AppCreateRequest;
 import com.wso2.http.HttpHandler;
 import org.apache.axis2.AxisFault;
@@ -11,6 +13,7 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.BasicConfigurator;
 import org.wso2.carbon.authenticator.stub.LoginAuthenticationExceptionException;
 import org.wso2.carbon.claim.mgt.stub.ClaimManagementServiceException;
+import org.wso2.carbon.registry.core.exceptions.RegistryException;
 import org.wso2.carbon.um.ws.api.stub.RemoteUserStoreManagerServiceUserStoreExceptionException;
 
 import java.io.IOException;
@@ -22,68 +25,92 @@ import java.rmi.RemoteException;
 public class MainController {
 
     private static final Log log = LogFactory.getLog(MainController.class);
+    private static WSRegistryService_Client wsRegistryService_client;
+    private static String backEndUrl = "https://localhost:9443";
+    private static HttpHandler httpHandler = new HttpHandler();
+    private static String  tomcatPort;
+    private static String  wampPort;
+    private  static String appmPath;
 
-    public static  void  main(String[] args){
+    public static void main(String[] args) {
         BasicConfigurator.configure();
-        publishApplication();
+        tomcatPort = args[0];
+        wampPort = args[1];
+        appmPath = args[2];
+        //log.info("Args 1 "+args[0]);
+        //log.info("Args 2 "+args[1]);
+        configure();
+
     }
 
 
-    private static  void publishApplication(){
-        System.setProperty("javax.net.ssl.trustStore", "/home/ushan/wso2/APPM/LATEST_BUILD/wso2appm-1.0.0-SNAPSHOT/repository/resources/security/wso2carbon.jks");
+    private static void configure() {
+        System.setProperty("javax.net.ssl.trustStore", appmPath+"/repository/resources/security/wso2carbon.jks");
         System.setProperty("javax.net.ssl.trustStorePassword", "wso2carbon");
         System.setProperty("javax.net.ssl.trustStoreType", "JKS");
-        String backEndUrl = "https://localhost:9443";
+
         LoginAdminServiceClient login = null;
 
         try {
             try {
-                Thread.sleep(1000);
+                Thread.sleep(10000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             login = new LoginAdminServiceClient(backEndUrl);
-            String session = login.authenticate("admin", "admin");
-            ClaimManagementServiceClient claimManagementServiceClient =  new ClaimManagementServiceClient(session,backEndUrl);
-            RemoteUserStoreManagerServiceClient remoteUserStoreManagerServiceClient =  new RemoteUserStoreManagerServiceClient(session,backEndUrl);
+            String normal_session = login.authenticate("admin", "admin");
+            ClaimManagementServiceClient claimManagementServiceClient =  new ClaimManagementServiceClient(normal_session,backEndUrl);
+            RemoteUserStoreManagerServiceClient remoteUserStoreManagerServiceClient =  new RemoteUserStoreManagerServiceClient(normal_session,backEndUrl);
             //plan-your-trip
             log.info("Add claim mapping");
             claimManagementServiceClient.addClaim("FrequentFlyerID", "http://wso2.org/ffid", true);
             claimManagementServiceClient.addClaim("zipcode", "http://wso2.org/zipcode", true);
             claimManagementServiceClient.addClaim("Credit card number", "http://wso2.org/claims/card_number",true);
-            claimManagementServiceClient.addClaim("zipcode","http://wso2.org/zipcode",true);
-            claimManagementServiceClient.addClaim("zipcode","http://wso2.org/zipcode",true);
+            claimManagementServiceClient.addClaim("Credit cArd Holder Name","http://wso2.org/claims/card_holder",true);
+            claimManagementServiceClient.addClaim("Credit card expiration date","http://wso2.org/claims/expiration_date",true);
 
             //filldata
             log.info("Updating claim values");
-            remoteUserStoreManagerServiceClient.updateClaims("admin","http://wso2.org/ffid","12345151");
-            remoteUserStoreManagerServiceClient.updateClaims("admin","http://wso2.org/claims/streetaddress","21/5");
-
-
+            remoteUserStoreManagerServiceClient.updateClaims("admin", "http://wso2.org/ffid", "12345151");
+            remoteUserStoreManagerServiceClient.updateClaims("admin", "http://wso2.org/claims/streetaddress", "21/5");
+            remoteUserStoreManagerServiceClient.updateClaims("admin", "http://wso2.org/zipcode", "GL");
+            remoteUserStoreManagerServiceClient.updateClaims("admin","http://wso2.org/claims/card_number", "001012676878");
+            remoteUserStoreManagerServiceClient.updateClaims("admin","http://wso2.org/claims/card_holder","Admin");
+            remoteUserStoreManagerServiceClient.updateClaims("admin","http://wso2.org/claims/expiration_date","31/12/2015");
             //publish application
             log.info("Creating application DTO");
-            AppCreateRequest appCreateRequest = new AppCreateRequest();
-            appCreateRequest.setOverview_name("travelWebapp");
-            appCreateRequest.setOverview_context("//travel");
-            appCreateRequest.setOverview_version("1.0.0");
-            appCreateRequest.setOverview_transports("http");
-            appCreateRequest.setOverview_webAppUrl("http://localhost:8080/plan-your-trip");
-
-            HttpHandler httpHandler = new HttpHandler();
             try {
-                String session_id =  httpHandler.doPost(backEndUrl + "/publisher/api/authenticate", "username=admin&password=admin&action=login", "");
-                log.info("Session id is : "+session_id);
+                String appm_session = httpHandler.doPost(backEndUrl + "/publisher/api/authenticate", "username=admin&password=admin&action=login", "","application/x-www-form-urlencoded");
+                AppCreateRequest appCreateRequest = new AppCreateRequest();
+                //publishing travelWebapp
+                appCreateRequest.setOverview_name("travelWebapp");
+                appCreateRequest.setOverview_context("/travel");
+                appCreateRequest.setOverview_version("1.0.0");
+                appCreateRequest.setOverview_transports("http");
+                appCreateRequest.setOverview_webAppUrl("http://localhost:"+tomcatPort+"/plan-your-trip");
                 String payload = appCreateRequest.generateRequestParameters();
-                log.info("Payload is : "+session_id);
-                String resPonce =   httpHandler.doPost(backEndUrl + "/publisher/asset/webapp", payload, session_id);
+                publishApplication(appCreateRequest.getOverview_name(), "webapp", normal_session, appm_session, payload,"application/x-www-form-urlencoded");
+                //publishing travel booking application
+                appCreateRequest.setOverview_name("TravelBooking");
+                appCreateRequest.setOverview_context("/travelBooking");
+                appCreateRequest.setOverview_version("1.0.1");
+                appCreateRequest.setOverview_transports("http");
+                appCreateRequest.setOverview_webAppUrl("http://localhost:"+tomcatPort+"/travelBooking");
+                payload = appCreateRequest.generateRequestParameters();
+                publishApplication(appCreateRequest.getOverview_name(), "webapp", normal_session, appm_session, payload,"application/x-www-form-urlencoded");
+                //publishing  CleanCalc mobile application
+
+                //appCreateRequest.set
+                //String responceBody = httpHandler.postMultiData(backEndUrl+"/publisher/api/mobileapp/upload","upload","",appm_session);
+                //httpHandler.postMultiData(backEndUrl+"/publisher/api/asset/mobileapp","none",responceBody,appm_session);
             } catch (IOException e) {
                 log.error(e.getMessage());
                 //e.printStackTrace();
             }
         } catch (AxisFault axisFault) {
             log.error(axisFault.getMessage());
-            publishApplication();
-           // axisFault.printStackTrace();
+            configure();
+            // axisFault.printStackTrace();
         } catch (RemoteException e) {
             //e.printStackTrace();
             log.error(e.getMessage());
@@ -95,12 +122,24 @@ public class MainController {
             //e.printStackTrace();
         } catch (RemoteUserStoreManagerServiceUserStoreExceptionException e) {
             log.error(e.getMessage());
-           // e.printStackTrace();
+            e.printStackTrace();
+        } catch (RegistryException e) {
+            e.printStackTrace();
         }
 
     }
 
 
+    private static void publishApplication(String applicationName, String applicationType, String session, String appm_session_id, String payload,String contentType) throws RegistryException, IOException {
+        String resPonce = httpHandler.doPost(backEndUrl + "/publisher/asset/" + applicationType, payload, appm_session_id,contentType);
+        wsRegistryService_client = new WSRegistryService_Client(backEndUrl, session);
+        String appPath = "/_system/governance/apimgt/applicationdata/provider/admin/" + applicationName + "/1.0.0/" + applicationType;
+        String UUID = wsRegistryService_client.getUUID(appPath);
+        //publishing application
+        httpHandler.RestPutClient(backEndUrl + "/publisher/api/lifecycle/Submit/"+applicationType+"/" + UUID, appm_session_id);
+        httpHandler.RestPutClient(backEndUrl + "/publisher/api/lifecycle/Approve/"+applicationType+"/" + UUID, appm_session_id);
+
+    }
 
 
 }
